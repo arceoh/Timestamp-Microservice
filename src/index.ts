6ff9dd7b5eaed2106611576331d2003d6df93d73
Hugo <arceoh@gmail.com>
2023-09-05 10:49:04 -0700

    Check if req is int

--- src/index.ts
+++ src/index.ts
@@ -10,31 +10,41 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
+
 app.get('/api/', (req: Request, res: Response) => {
-        const currentTime = {
-            unix: Date.now(),
-            utc: new Date().toUTCString()
-        }
-        res.send(currentTime)
-        console.log(currentTime);
+    const currentTime = {
+        unix: Date.now(),
+        utc: new Date().toUTCString()
+    }
+    res.send(currentTime)
 });
 
 app.get('/api/:date', (req: Request, res: Response) => {
 
+    if(req.params.date) { 
 
     
-    if(req.params.date) {
-      try {
-        const date = new Date(req.params.date)
+        // let allInts =   req.params.date.trim().split('').every(item => parseInt(item)) // Check if all characters are integers
+
+
+       // async .every method
+        const allInts =  req.params.date.trim().split('').every(async item => {
+            const isInt = await parseInt(item)
+            return isInt
+        }) // Check if all characters are integers
+
+        const date = allInts ? new Date(parseInt(req.params.date)) :  new Date(`${req.params.date}`)  
         const utc = date.toUTCString()
-        const unix = date.getTime()
-        res.send({unix, utc})
-      }
-      catch {
-        res.send({error: "Invalid Date"})
-      }
+        const unix = date.getTime()       
+        res.send({
+            unix: unix,
+            utc: utc
+        })
+    
     }
 });
+
+
 
 
 
